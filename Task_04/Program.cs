8996312176ab9aee3c15c2f276318e9965f42b4d
SSKos <sskavr@gmail.com>
2022-07-08 23:14:22 +0300

    Решена задача 04

---
+++ Task_04/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число ");
+int num = int.Parse(Console.ReadLine());
+int even = num % 2;
+if (even == 0) { 
+Console.Write("Число " + num + " четное"); }
+else {
+Console.Write("Число " + num + " нечетное");
+}
