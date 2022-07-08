b7c896ac96177e36311ab15bb386037ff110f325
SSKos <sskavr@gmail.com>
2022-07-08 23:09:11 +0300

    Решена задача 03

---
+++ Task_03/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число ");
+int num2 = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число ");
+int num3 = int.Parse(Console.ReadLine());
+int max = num1;
+if (max < num2) { 
+    max = num2;
+ }
+if (max < num3) { 
+        max = num3;
+ }
+Console.Write("Максимальное число " + max);
