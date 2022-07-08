0de747b6c3bff1d50efa87e5c6cebf14416da2dc
SSKos <sskavr@gmail.com>
2022-07-08 23:20:06 +0300

    Решена задача 05

---
+++ Task_05/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число ");
+int num = int.Parse(Console.ReadLine());
+for (int i = 1; i <= num; i++)
+{
+    int even = i % 2;
+    if (even == 0)
+    {
+        Console.Write(i + " ");
+    }
+}
